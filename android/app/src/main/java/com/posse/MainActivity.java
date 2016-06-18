139821e4873ab3dff2a80b18e361681374e0e80c
twillzy <william.tanzy@gmail.com>
2016-06-18 14:43:00 +1000

    facebook login ready!

--- android/app/src/main/java/com/posse/MainActivity.java
+++ android/app/src/main/java/com/posse/MainActivity.java
@@ -1,6 +1,10 @@
 package com.posse;
 
+import android.content.Intent;
+
+import com.facebook.CallbackManager;
 import com.facebook.react.ReactActivity;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -8,6 +12,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ReactActivity {
+
+    CallbackManager mCallbackManager;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -33,8 +39,18 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
-        );
+        mCallbackManager = new CallbackManager.Factory().create();
+        ReactPackage packages[] = new ReactPackage[] {
+            new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
+        };
+
+        return Arrays.<ReactPackage>asList(packages);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ android/app/src/main/java/com/posse/MyApplication.java
@@ -0,0 +1,14 @@
+package com.posse;
+
+import android.app.Application;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
